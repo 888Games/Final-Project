1d7a8378603efd8c9a512f51251467acee10efa3
Simon Florian <>
2019-12-20 22:28:08 +0100

    added javadoc for game mechanics

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+ *
+ * @author Simon Martin Florian
+ */
+
+import java.util.List;
+
+/**
+ * Class for creating a game and starting rounds
+ */
+
+public class Game {
+
+    /**
+     * Constructor for creating a new game
+     * @param gameRules List of rules for the game also used to evaluate the winner
+     * @param name String value of the name of the game
+     */
+
+    public Game(List<GameRules> gameRules, String name){
+
+    }
+
+    /**
+     * Method for creating a new game round in the game
+     */
+    public void newGameRound() {
+
+    }
+}
---
+++ src/main/java/model/GameRound.java
@@ -0,0 +1,30 @@
+package model;
+
+/**
+ *
+ * @author Simon Martin Florian
+ */
+
+/**
+ * Class for a game round of a specific game
+ */
+
+public class GameRound {
+
+    /**
+     * Method for evaluating the winner of a specific game round
+     * @return String id of the gamblerCard id of the winner
+     */
+
+    public String evaluateWinner() {
+        return "";
+    }
+
+
+    /**
+     * Method for starting a new betting round in a game round
+     */
+    public void addBettingRound() {
+
+    }
+}
