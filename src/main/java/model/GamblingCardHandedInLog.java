b3f75470dfdb6a6e7b471406b0d56680ef1a24ca
Ulrich971 <ulrich.firpion@isep.fr>
2019-12-20 22:50:37 +0100

    GamblingCardHanded log for giving and returning

---
+++ src/main/java/model/GamblingCardHandedInLog.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ * @author Ulrich Firpion
+ */
+
+/**
+ * Class to manage when a GamblingCard is handed by the cashier
+ */
+
+
+public class GamblingCardHandedInLog {
+
+
+    /**
+     * Override method which displays when a gamblerCard has been handed by the cashier to a gambler
+     * @return info
+     */
+     @Override
+    public String toString() {
+        return "GamblerCard has been given to the gambler" ;
+    }
+}
