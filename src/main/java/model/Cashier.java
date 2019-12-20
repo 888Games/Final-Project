4badfac5d30f8330525d468e0c9668d1efa3395d
Simon Florian <>
2019-12-20 21:49:11 +0100

    added javadoc for gambler card and cashier

--- src/main/java/model/Cashier.java
+++ src/main/java/model/Cashier.java
@@ -1,4 +1,56 @@
 package model;
 
+/**
+ * @author Simon Martin Florian*
+ */
+
+/**
+ * Class to manage gambler cards
+ */
+
 public class Cashier {
+
+    /**
+     * This is a method for getting the current credit of a GamblerCard.
+     * @param gambleCardId String value of the gambleCard id
+     * @return currentCredit Double value of the current credit for the card id
+     */
+
+    public double getCredit(String gambleCardId){
+        return 0.0;
+    }
+
+
+    /**
+     * This is a method for giving a gambler card back and getting the credit from the card
+     * @param gambleCardId String value of the gambleCard id
+     * @return credit Double value of the balance for the card id
+     */
+
+    public double cashOutCard(String gambleCardId) {
+        return 0.0;
+    }
+
+    /**
+     * This is a method for creating a new a gambler card
+     * @param initialBalance Double value of the initial balance
+     * @return String value of the id of the created gambler card
+     */
+
+    public String getGamblerCard(double initialBalance){
+        return "";
+    }
+
+
+    /**
+     * This is a method for updating the current credit of a gambler card
+     * @param gambleCardId String value of the gambleCard id
+     * @param credit Double value of the credit which is added or substracted from the current credit
+     */
+
+    public void updateCredit(String gambleCardId, double credit){
+
+    }
+
+
 }
---
+++ src/main/java/model/GamblerCard.java
@@ -0,0 +1,43 @@
+package model;
+
+/**
+ *
+ * @author Simon Martin Florian
+ */
+
+/**
+ * Class for gamblerCards
+ */
+
+public class GamblerCard {
+
+    /**
+     * Constructor for creating a gamblerCard
+     * @param initialBalance Double value of the initial balance
+     * Id is automatically created
+     */
+
+    public GamblerCard(double initialBalance){
+
+    }
+
+    /**
+     * Method to store a betId on the card
+     * @param betId String value of the bet Id
+     */
+
+    public void addBetId(String betId) {
+
+    }
+
+    /**
+     * Method to generate a new betId
+     * @return  betId String value of the generated bet Id
+     */
+
+    public String generateBetId() {
+        return "";
+    }
+
+
+}
