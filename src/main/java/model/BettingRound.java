63acdce2afcdb9dd1a29ec9f195031c967fb7c60
memmm <emesenge@gmail.com>
2019-12-20 22:00:02 +0100

    Added classes for Bet, Machine and BettingRound

---
+++ src/main/java/model/BettingRound.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ *
+ */
+public class BettingRound {
+
+    /**
+     * The passed value will be
+     * @param inValue
+     */
+    public void placeBet(int inValue) {}
+
+
+    /**
+     *
+     * @param bet
+     */
+    public void getOutValue(Bet bet) {}
+}
+
