40e851dc9634721e8ae8b0f74103de9d7aecdf86
memmm <emesenge@gmail.com>
2020-01-19 23:24:33 +0100

    Auto stash before revert of "created tests for Logger"

--- src/test/java/model/LoggerTest.java
+++ src/test/java/model/LoggerTest.java
@@ -3,10 +3,68 @@ package model;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class LoggerTest {
 
     @Test
     public void log() {
+    }
+
+    /**
+     * Asserts if log method stores a new log in logs list.
+     */
+    @Test
+    public void logs_AfterCreationOfLog_ShouldContainLog() {
+        //arrange
+        Logger logger = new Logger();
+        String message = "Almost before we knew it, we had left the ground.";
+        //act
+        logger.log(message);
+        //assert
+        assertTrue(logger.getLogs().contains(message));
+    }
+
+    /**
+     * Asserts whether trying to store a null message throws IllegalArgumentException.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void logMessage_WithNullMessage_ShouldThrowIllegalArgumentsException() {
+        //arrange
+        Logger logger = new Logger();
+        String message = null;
+        //act
+        logger.log(message);
+    }
+
+    /**
+     * Asserts whether an empty message is successfully stored in logs.
+     */
+    @Test
+    public void logMessage_WithEmptyMessage_ShouldPass() {
+        //arrange
+        Logger logger = new Logger();
+        String message = "";
+        //act
+        logger.log(message);
+        //assert
+        assertTrue(logger.getLogs().contains(message));
+    }
+
+    /**
+     * Checks if logging a message also means sending it to the AuthorityGateway.
+     * Indirect output
+     */
+    @Test
+    public void log_WhenLoggingMessage_ShouldNotifyAuthority() {
+        //arrange
+        Logger logger = new Logger();
+        AuthorityGateway ag = mock(AuthorityGateway.class);
+        String message = "A shining crescent far beneath the flying vessel.";
+        //act
+        logger.log(message);
+        //assert
+        verify(ag).sendLog(message);
     }
 }
