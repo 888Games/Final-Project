7cfd0f94565440edafd20152c51ca0b8c19f8e59
Ulrich971 <ulrich.firpion@isep.fr>
2020-01-19 17:28:45 +0100

    Remove test for resolve Bet

--- src/test/java/model/BetTest.java
+++ src/test/java/model/BetTest.java
@@ -32,25 +32,7 @@ public class BetTest {
         Bet bet = new Bet(2.0,null,card);
     }
 
-    @Test
-    public void resolveBet_WithZeroOutValue_ShouldPass(){
-
-    }
-
-    @Test
-    public void resolveBet_WithPositiveOutValue_ShouldPass(){
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void resolveBet_WithNegativeOutValue_ShouldThrowIllegalArgumentsException(){
-
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void resolveBet_WithNullOutValue_ShouldThrowIllegalArgumentsException(){
-
-    }
+ 
 
 
 }
