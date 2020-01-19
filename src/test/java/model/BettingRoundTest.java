78e6159279fa99c312d005d163e3cb6213b84390
Ulrich971 <ulrich.firpion@isep.fr>
2020-01-19 19:27:39 +0100

    Correction Created placeBet_WithNotResolvedBet_ShouldPass test

--- src/test/java/model/BettingRoundTest.java
+++ src/test/java/model/BettingRoundTest.java
@@ -17,6 +17,18 @@ public class BettingRoundTest {
         assertTrue(bettingRound != null);
     }
 
+    @Test
+    public void placeBet_WithNotResolvedBet_ShouldPass(){
+        //arrange
+        BettingRound bettingRound = new BettingRound();
+        Bet bet = mock(Bet.class);
+        when(bet.isResolved()).thenReturn(Boolean.FALSE);
+
+        //act
+        bettingRound.placeBet(bet);
+
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void placeBet_WithResolvedBet_ShouldThrowIllegalArgumentsException(){
         //arrange
