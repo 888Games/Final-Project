250be51dce15f87a9b2ab0fd93596551c42a370f
Maurici <hello@mauriciabad.com>
2020-01-19 23:09:56 +0100

    Create all tests for Cashier without implementation

--- src/test/java/model/CashierTest.java
+++ src/test/java/model/CashierTest.java
@@ -6,23 +6,167 @@ import static org.junit.Assert.*;
 
 public class CashierTest {
 
-    @Test
-    public void createCard() {
+    /**
+     * getInstance_Always_ShouldPass
+     */
+   @Test
+    public void getInstance_Always_ShouldPass() {
+
     }
 
+    /**
+     * getInstance_Twice_ShouldReturnTheSameInstance
+     */
     @Test
-    public void getCardCredit() {
+    public void getInstance_Twice_ShouldReturnTheSameInstance() {
+
     }
 
+
+    /**
+     * createCard_WithPositiveInitialBalance_ShouldPass
+     */
     @Test
-    public void updateCardCredit() {
+    public void createCard_WithPositiveInitialBalance_ShouldPass() {
+
     }
 
+    /**
+     * createCard_WithZeroInitialBalance_ShouldPass
+     */
     @Test
-    public void checkCardCredit() {
+    public void createCard_WithZeroInitialBalance_ShouldPass() {
+
     }
 
+    /**
+     * createCard_WithNegativeInitialBalance_ShouldThrowIllegalArgumentsException
+     */
     @Test
-    public void cashOutCard() {
+    public void createCard_WithNegativeInitialBalance_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+    /**
+     * createCard_WithNullInitialBalance_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void createCard_WithNullInitialBalance_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+
+    /**
+     * getCardCredit_ShouldPass
+     */
+    @Test
+    public void getCardCredit_ShouldPass() {
+
+    }
+
+    /**
+     * getCardCredit_AfterUpdatingCredit_ShouldUpdateTheReturnValue
+     */
+    @Test
+    public void getCardCredit_AfterUpdatingCredit_ShouldUpdateTheReturnValue() {
+
+    }
+
+    /**
+     * getCardCredit_WithNullCard_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void getCardCredit_WithNullCard_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+    /**
+     * getCardCredit__WithACardNotCreatedByTheCashier_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void getCardCredit__WithACardNotCreatedByTheCashier_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+
+    /**
+     * cashOutCard_ShouldPass
+     */
+    @Test
+    public void cashOutCard_ShouldPass() {
+
+    }
+
+    /**
+     * cashOutCard__WithNullCard_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void cashOutCard__WithNullCard_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+    /**
+     * cashOutCard__WithACardNotCreatedByTheCashier_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void cashOutCard__WithACardNotCreatedByTheCashier_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+
+    /**
+     * updateCardCredit_ShouldPass
+     */
+    @Test
+    public void updateCardCredit_ShouldPass() {
+
+    }
+
+    /**
+     * updateCardCredit_WithNullCard_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void updateCardCredit_WithNullCard_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+    /**
+     * updateCardCredit_WithNullCredit_ShouldThrowIllegalArgumentsException
+     */
+    @Test
+    public void updateCardCredit_WithNullCredit_ShouldThrowIllegalArgumentsException() {
+
+    }
+
+    /**
+     * updateCardCredit_WithPositiveCredit_ShouldPass
+     */
+    @Test
+    public void updateCardCredit_WithPositiveCredit_ShouldPass() {
+
+    }
+
+    /**
+     * updateCardCredit_WithZeroCredit_ShouldPass
+     */
+    @Test
+    public void updateCardCredit_WithZeroCredit_ShouldPass() {
+
+    }
+
+    /**
+     * updateCardCredit_WithNegativeCredit_ShouldPass
+     */
+    @Test
+    public void updateCardCredit_WithNegativeCredit_ShouldPass() {
+
+    }
+
+    /**
+     * updateCardCredit_WithACardThatDoesntHaveEnoughCredit_ShouldThrowInsufficientCreditException
+     */
+    @Test
+    public void updateCardCredit_WithACardThatDoesntHaveEnoughCredit_ShouldThrowInsufficientCreditException() {
+
     }
 }
