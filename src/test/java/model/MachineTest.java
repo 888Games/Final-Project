4503e884119901ca1d2ab942dc269660580b91de
Simon Florian <>
2020-01-19 13:37:49 +0100

    created createMachine_Always_ShouldPass test

--- src/test/java/model/MachineTest.java
+++ src/test/java/model/MachineTest.java
@@ -2,27 +2,16 @@ package model;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class MachineTest {
 
     @Test
-    public void placeBet() {
-    }
-
-    @Test
-    public void getOutValue() {
-    }
-
-    @Test
-    public void connectCard() {
-    }
-
-    @Test
-    public void disconnectCard() {
-    }
-
-    @Test
-    public void givePrize() {
+    public void createMachine_Always_ShouldPass() {
+        // arrange
+        Machine machine = new Machine();
+        //assert
+        assertTrue(machine != null);
     }
 }
