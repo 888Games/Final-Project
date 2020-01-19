ea6c00dfe00a2534d596c2a7ce50910d09fa3889
Ulrich971 <ulrich.firpion@isep.fr>
2020-01-19 22:21:15 +0100

    Created code for createCard_Always_ShouldPass test

--- src/test/java/model/CardTest.java
+++ src/test/java/model/CardTest.java
@@ -7,10 +7,7 @@ import static org.junit.Assert.*;
 public class CardTest {
 
     @Test
-    public void addBetId() {
-    }
-
-    @Test
-    public void generateBetId() {
+    public void createCard_Always_ShouldPass(){
+        Card card = new Card();
     }
 }
