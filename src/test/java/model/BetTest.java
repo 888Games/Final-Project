7f6c166b9ce6326359dc48a0dfd72db5a3b1be5a
Ulrich971 <ulrich.firpion@isep.fr>
2020-01-19 17:14:24 +0100

    Created createBet_WithNonNullValues_ShouldPass test

--- src/test/java/model/BetTest.java
+++ src/test/java/model/BetTest.java
@@ -1,12 +1,52 @@
 package model;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.Assert.*;
 
 public class BetTest {
 
     @Test
-    public void resolve() {
+    public void createBet_WithNonNullValues_ShouldPass(){
+        //arrange
+        Card card = mock(Card.class);
+        Machine machine = mock(Machine.class);
+
+        //act
+        Bet bet = new Bet(2.0,machine,card);
+
+        //assert
+        assertTrue(bet != null);
+
     }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createBet_WithNullValues_ShouldThrowIllegalArgumentsException(){
+
+    }
+
+    @Test
+    public void resolveBet_WithZeroOutValue_ShouldPass(){
+
+    }
+
+    @Test
+    public void resolveBet_WithPositiveOutValue_ShouldPass(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void resolveBet_WithNegativeOutValue_ShouldThrowIllegalArgumentsException(){
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void resolveBet_WithNullOutValue_ShouldThrowIllegalArgumentsException(){
+
+    }
+
+
 }
